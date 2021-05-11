af399f4e289724401bfa9a45be5ac252bce6e4d6
jkelly101 <jenLkelly@gmail.com>
2021-05-11 18:53:56 -0400

    set up pages and components

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Projects from "./components/pages/Projects";
+import Contact from "./components/pages/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
